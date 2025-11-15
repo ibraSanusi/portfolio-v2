cae6ba0b6cfc726bfb4d6c5ea8a7c0ee62d7a95c
ibraSanusi <isanu227@gmail.com>
2025-11-15 12:37:03 +0100

    feat: install vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import vercelAdapter from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: vercelAdapter(),
   vite: {
     plugins: [tailwindcss()]
   }
